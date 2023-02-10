f42d63d3ff3b0ef77ce6e51c205bd884bf34f4a0
Jacob Sniff <jacobsniff18@gmail.com>
2023-02-10 15:21:14 -0500

    password with the correct criteria is generating, but it is adding one more character than it should be.

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passLength = [];
-let arr = [];
-for (var i = 8; i < 128; i++) {
-    passLength.push(i);
-}
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChar = " !'()*+#,%-./:&;$<=>?@[]^_`{|}~,";
+var numChar = "0123456789";
 
+function generatePassword() {
+  var userPass = " ";
+  var emptyString = " ";
+  var passLength = parseInt(prompt("Please enter the desired length of your password. (Must be 8 - 128)"));
+  if (!passLength) {
+    return "";
+  }
+  else if (passLength < 8 || passLength > 128) {
+    window.alert("Please enter a length within the parameters.");
+    return "";
+  }
+  var lowerChoice = confirm("Please confirm if you would like to include lowercase characters.")
+  if (lowerChoice === true) {
+    emptyString += lowerChar;
+  }
+  var upperChoice = confirm("Please confrim if you would like to include uppercase characters.")
+  if (upperChoice === true) {
+    emptyString += upperChar;
+  }
+  var specialChoice = confirm("Please confirm if you would like to include special characters.")
+  if (specialChoice === true) {
+    emptyString += specialChar;
+  }
+  var numberChoice = confirm("Please confirm if you would like to include numbers.")
+  if (numberChoice === true) {
+    emptyString += numChar;
+  }
+  if (lowerChoice === false && upperChoice === false && specialChoice === false && numberChoice === false) {
+    alert("You must include at least one of the password variables.")
+    return "";
+  }
+
+  for (var i = 0; i <= passLength; i++) {
+    var placeHolder = emptyString[Math.floor(Math.random() * emptyString.length)];
+    userPass += placeHolder;
+  }
+  return userPass;
+}
 
 
 // Write password to the #password input
@@ -19,9 +56,9 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", e => {
-  window.prompt( "Please enter the desired length of your password. (Must be 8 - 128)");
-})
+generateBtn.addEventListener("click", writePassword);
+
+
 
 
 // add to buttons ------> onclick= "passCriteria()
@@ -30,6 +67,11 @@ generateBtn.addEventListener("click", e => {
 // }
 
 
+// var passLength = [];
+// for (var i = 8; i < 128; i++) {
+//     passLength.push(i);
+// }
+
 
 
 
@@ -37,12 +79,12 @@ generateBtn.addEventListener("click", e => {
 
 function myFunction() {
   var element = document.body;
-  element.classList.toggle( "dark-mode" );
+  element.classList.toggle("dark-mode");
 }
 
 
 let button = document.querySelector('button');
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
   console.log('Button was clicked!');
 });
